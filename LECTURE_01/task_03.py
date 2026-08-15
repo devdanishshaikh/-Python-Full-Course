8a2641384f1f361a07a9c41aed59ec2d81341c1a
devdanishshaikh <shaikhdani710@gmail.com>
2026-08-15 14:09:31 +0500

    Lecture 01 Tasks

--- LECTURE_01/task_03.py
+++ LECTURE_01/task_03.py
@@ -1,4 +1,4 @@
 #take input side of square from user and print area of square
-side = int(input("Enter side of square :"))
+side = float(input("Enter side of square :"))
 area = side * side
 print(f"Area of square with side {side} is : ", area)
---
+++ LECTURE_01/task_04.py
@@ -0,0 +1,5 @@
+#find average of two number by taking user input
+x = float(input("Enter 1st number : "))
+y = float(input("Enter 2nd number : "))
+avg = (x+y)/2
+print(f"average of {x} and {y} is :", avg)
