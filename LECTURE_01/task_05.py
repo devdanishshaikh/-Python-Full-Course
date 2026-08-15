9800639940e8d784ed8ceb67e4e4652f2d11714d
devdanishshaikh <shaikhdani710@gmail.com>
2026-08-15 14:17:05 +0500

    Task_05 complete

---
+++ LECTURE_01/task_05.py
@@ -0,0 +1,4 @@
+#take 2 number  a b as user input and if a is greater print true if not print false
+a = int(input("Enter first number :" ))
+b = int(input("Enter second number : "))
+print(a > b and b < a)
